11ddf2d6a6f051841a442078344d7749a66256ac
Heather Peterson <hpeterson462@gmail.com>
2020-11-04 09:37:15 -0800

    initial commit

---
+++ src/components/app/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Switch, Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+export default function App() {
+  return (
+    <>
+      <Provider>
+        <Switch>
+
+        </Switch>
+      </Provider>
+    </>
+  )
+}
